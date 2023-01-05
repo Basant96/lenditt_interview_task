d3dd87a13efb9613239fa6e1ae9856cdec9a5089
crazyyman <royalbasantbanna@gmail.com>
2023-01-05 14:33:29 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,17 @@ import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { tableData } from './model';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements AfterViewInit{
+export class AppComponent implements AfterViewInit {
   title = 'taskAngularBasant';
-
+ 
+  from = '';
+  to = '';
   displayedColumns: string[] = ['name', 'creation_date', 'type'];
   dataSource = new MatTableDataSource<any>(tableData);
   @ViewChild(MatPaginator) paginator: MatPaginator;
@@ -25,17 +28,41 @@ export class AppComponent implements AfterViewInit{
     this.dataSource.sort = this.sort;
 
   }
-
+ 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
-  }
-  constructor(){
-   
-  }
-  ngOnInit(): void {
-    
+
   }
 
- 
+  filterTable() {
+    this.dataSource = new MatTableDataSource<any>(
+      tableData.filter((x) => {
+        console.log('this.to', x.creation_date);
+        if (
+          new Date(x.creation_date) >= new Date(this.from) &&
+          new Date(x.creation_date) <= new Date(this.to)
+        ) {
+          return true;
+        }
+      })
+    );
+  }
+
+  formateDate(date: Date) {
+    return (
+      date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate()
+    );
+  }
+
+
+
+  constructor() {
+
+  }
+  ngOnInit(): void {
+
+  }
+
+
 }
--- src/app/model.ts
+++ src/app/model.ts
@@ -1,186 +1,186 @@
  export const tableData= [
     {
         name: 'basant', 
-        creation_date: '11/10/2022',
+        creation_date: new Date('01/11/2022'),
         type: 'User'
     },
     {
         name: 'basantsingh', 
-        creation_date: '15/10/2022',
+        creation_date: new Date('08/10/2022'),
         type: 'Admin'
     },
     {
-        name: 'balveersingh', 
-        creation_date: '20/10/2022',
+        name: 'shekhawat', 
+        creation_date: new Date('09/01/2022'),
         type: 'User'
     },
     {
-        name: 'singhbalveer', 
-        creation_date: '22/10/2022',
+        name: 'singhsaab', 
+        creation_date: new Date('12/25/2022'), 
         type: 'User'
     },
     {
         name: 'ravi', 
-        creation_date: '25/10/2022',
+        creation_date: new Date('10/25/2022'),
         type: 'Admin'
     },
     {
         name: 'raju', 
-        creation_date: '28/10/2022',
+        creation_date: new Date('10/10/2022'),
         type: 'User'
     },
     {
         name: 'kaju', 
-        creation_date: '30/10/2022',
+        creation_date: new Date('11/10/2022'),
         type: 'Admin'
     },
     {
         name: 'dilp', 
-        creation_date: '1/11/2022',
+        creation_date: new Date('1/11/2022'),
         type: 'User'
     },
     {
         name: 'basant', 
-        creation_date: '5/11/2022',
+        creation_date: new Date('5/11/2022'),
         type: 'Admin'
     },
     {
         name: 'yuraj', 
-        creation_date: '10/11/2022',
+        creation_date: new Date('10/11/2022'),
         type: 'User'
     },
     {
         name: 'payal', 
-        creation_date: '15/11/2022',
+        creation_date: new Date('12/11/2022'),
         type: 'Admin'
     },
     {
         name: 'chitra', 
-        creation_date: '20/11/2022',
+        creation_date: new Date('11/11/2022'),
         type: 'User'
     },
     {
         name: 'sunita', 
-        creation_date: '25/10/2022',
+        creation_date: new Date('10/10/2022'),
         type: 'Admin'
     },
     {
         name: 'khusbhu', 
-        creation_date: '11/10/2022',
+        creation_date: new Date('09/10/2022'),
         type: 'Admin'
     },
 
     {
         name: 'riddhi', 
-        creation_date: '11/10/2022',
+        creation_date: new Date('11/10/2022'),
         type: 'User'
     },
     {
         name: 'bhuatik', 
-        creation_date: '11/10/2022',
+        creation_date: new Date('11/10/2022'),
         type: 'Admin'
     },
 
     {
         name: 'suman', 
-        creation_date: '11/10/2022',
+        creation_date: new Date('11/10/2022'),
         type: 'User'
     },
     {
         name: 'sandeep', 
-        creation_date: '11/10/2022',
+        creation_date: new Date('11/10/2022'),
         type: 'Admin'
     },
     {
         name: 'basant', 
-        creation_date: '11/10/2022',
+        creation_date: new Date('11/10/2022'),
         type: 'User'
     },
     {
         name: 'basantsingh', 
-        creation_date: '15/10/2022',
+        creation_date: new Date('08/10/2022'),
         type: 'Admin'
     },
     {
-        name: 'balveersingh', 
-        creation_date: '20/10/2022',
+        name: 'balramsingh', 
+        creation_date: new Date('07/10/2022'),
         type: 'User'
     },
     {
-        name: 'singhbalveer', 
-        creation_date: '22/10/2022',
+        name: 'singhbalram', 
+        creation_date: new Date('12/12/2022'),
         type: 'User'
     },
     {
         name: 'ravi', 
-        creation_date: '25/10/2022',
+        creation_date: new Date('12/12/2022'),
         type: 'Admin'
     },
     {
         name: 'raju', 
-        creation_date: '28/10/2022',
+        creation_date: new Date('10/10/2022'),
         type: 'User'
     },
     {
         name: 'kaju', 
-        creation_date: '30/10/2022',
+        creation_date: new Date('11/10/2022'),
         type: 'Admin'
     },
     {
         name: 'dilp', 
-        creation_date: '1/11/2022',
+        creation_date: new Date('01/11/2022'),
         type: 'User'
     },
     {
         name: 'basant', 
-        creation_date: '5/11/2022',
+        creation_date: new Date('05/11/2022'),
         type: 'Admin'
     },
     {
         name: 'yuraj', 
-        creation_date: '10/11/2022',
+        creation_date: new Date('10/11/2022'),
         type: 'User'
     },
     {
         name: 'payal', 
-        creation_date: '15/11/2022',
+        creation_date: new Date('06/11/2022'),
         type: 'Admin'
     },
     {
         name: 'chitra', 
-        creation_date: '20/11/2022',
+        creation_date: new Date('07/11/2022'),
         type: 'User'
     },
     {
         name: 'sunita', 
-        creation_date: '25/10/2022',
+        creation_date: new Date('08/10/2022'),
         type: 'Admin'
     },
     {
         name: 'khusbhu', 
-        creation_date: '11/10/2022',
+        creation_date: new Date('11/10/2022'),
         type: 'Admin'
     },
 
     {
         name: 'riddhi', 
-        creation_date: '11/10/2022',
+        creation_date: new Date('11/10/2022'),
         type: 'User'
     },
     {
         name: 'bhuatik', 
-        creation_date: '11/10/2022',
+        creation_date: new Date('11/10/2022'),
         type: 'Admin'
     },
 
     {
         name: 'suman', 
-        creation_date: '11/10/2022',
+        creation_date: new Date('11/10/2022'),
         type: 'User'
     },
     {
         name: 'sandeep', 
-        creation_date: '11/10/2022',
+        creation_date: new Date('11/10/2022'),
         type: 'Admin'
     },
    
